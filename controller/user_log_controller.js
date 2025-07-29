7e100e6ee8fa5fa30ef6265e90bdf03c6a492c9f
Dia505 <dia48261@gmail.com>
2025-07-30 03:10:14 +0545

    Limited user logs to latest 100 request

--- controller/user_log_controller.js
+++ controller/user_log_controller.js
@@ -2,7 +2,7 @@ const UserLog = require("../model/user_log");
 
 const findAll = async (req, res) => {
   try {
-    const userLog = await UserLog.find();
+    const userLog = await UserLog.find().sort({ timestamp: -1 }).limit(100);;
     res.status(200).json(userLog);
   }
   catch (e) {
